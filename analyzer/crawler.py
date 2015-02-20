2719ace62499c313dd5502312b443556e94281c0
Igal Kreichman <igal@skycure.com>
2015-02-20 15:05:08 +0200

    Populate members

--- analyzer/crawler.py
+++ analyzer/crawler.py
@@ -1,10 +1,11 @@
 import json
+import re
 import time
 import traceback
 import urllib
 from collections import Iterator
 from progressbar import ProgressBar
-from analyzer.db_models.member import Member
+from analyzer.db_models.member import Member, SkippedMember
 from analyzer.db_models.party import Party
 
 DOMAIN = 'http://oknesset.org'
@@ -50,9 +51,21 @@ class ResourceFactory(JsonResource):
     return Resource(self.resources_json[name]['list_endpoint'])
 
 
-class Resource(JsonResource, Iterator):
+class ResourceItem(JsonResource):
   def __init__(self, url):
     self.data = self._get_json_data(url)
+
+  def __getitem__(self, item):
+    return self.data[item]
+
+  @staticmethod
+  def extend(item):
+    return ResourceItem(item['resource_uri'])
+
+
+class Resource(ResourceItem, Iterator):
+  def __init__(self, url):
+    ResourceItem.__init__(self, url)
     self.current = 0
 
   def next(self):
@@ -84,36 +97,44 @@ class OKnessetCrawler:
   def __init__(self):
     self.parties = {party.id: party for party in Party.objects.all()}
     self.members = {member.id: member for member in Member.objects.all()}
+    self.skipped_members = [m.id for m in SkippedMember.objects.all()]
 
   def populate_all(self):
     self.populate_parties()
     self.populate_members()
 
   def populate_parties(self):
-    self._populate_resource('party', self.parties, self.populate_party)
+    self._populate_resource('party', self.parties, [], self.populate_party)
 
   def populate_party(self, item):
     return Party.from_json(item)
 
   def populate_members(self):
-    self._populate_resource('member', self.members, self.populate_member)
+    self._populate_resource('member', self.members, self.skipped_members, self.populate_member)
 
   def populate_member(self, item):
-    member = Member.from_json(item)
+    extended = ResourceItem.extend(item)
+    party_id = int(re.match('/party/(\\d+)/', extended['party_url']).group(1))
+    if party_id not in self.parties:
+      SkippedMember.objects.create(id=item['id']).save()
+      return None
+
+    member = Member.from_json(item, party=self.parties[party_id])
     return member
 
   @staticmethod
-  def _populate_resource(resource, container, populator):
+  def _populate_resource(resource, container, skip_list, populator):
     print '='*80
     print 'Fetching %s...' % resource
     data = Resource.get(resource)
     progress = ProgressBar()
     for item in progress(data):
-      if item['id'] in container:
+      if item['id'] in container or item['id'] in skip_list:
         continue
       try:
         model = populator(item)
-        container[model.id] = model
+        if model is not None:
+          container[model.id] = model
       except Exception, e:
         print e
         traceback.print_exc()
