688185fae635a8371ab8de23397a7f2ac9199d91
Nadir Izrael <nadir.izr@gmail.com>
2015-02-20 15:13:30 +0200

    Always delete previous objects when creating new ones.

--- analyzer/db_models/base.py
+++ analyzer/db_models/base.py
@@ -4,6 +4,9 @@ from django.db import models
 class BaseModel(models.Model):
   @classmethod
   def from_json(cls, data):
+    if "id" in data:
+      cls.objects.filter(id=data["id"]).delete()
+
     filtered = { k: v for (k, v) in data.items()
                  if k in cls._meta.get_all_field_names() }
     instance = cls.objects.create(**filtered)
